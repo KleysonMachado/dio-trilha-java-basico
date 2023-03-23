3f17af1d56260faa5526c7434d94c6629920e66a
KleysonMachado <kleysonm@gmail.com>
2023-03-23 15:39:07 -0300

    Fifth commit

---
+++ src/tipos_variaveis/TipoDados.java
@@ -0,0 +1,16 @@
+package tipos_variaveis;
+
+public class TipoDados {
+
+	public static void main(String[] args) {
+		byte idade = 123;
+		short ano = 2023;
+		int cep = 71926000; //se começar com zero, talvez tenha que ser outro tipo
+		long cpf = 12345678900L; // se começar com zero,  talvez tenha que ser outro tipo
+		float pi = 3.14F;
+		double salario = 1275.33;
+		String meuNome = "Kleyson Machado";
+
+	}
+
+}
