95d62231435a5232df10dd24c65e28aa9bba60c6
KleysonMachado <kleysonm@gmail.com>
2023-03-27 09:56:59 -0300

    First commit dia 27 de março de 2023.

---
+++ src/tipos_variaveis/Operadores.java
@@ -0,0 +1,32 @@
+package tipos_variaveis;
+
+public class Operadores {
+
+	public static void main(String[] args) {
+		int numero = 5;
+		//repetição
+		
+		//numero = numero + 1;
+		numero++;
+		System.out.println(numero);
+		
+		
+		int a, b;
+		a = 6;
+		b = 6;
+		
+		String resultado = a==b ?"verdadeiro" : "falso";	
+		/*
+		if(a==b)
+			resultado = "verdadeiro";
+		else
+			resultado = "falso";
+		*/
+		
+		System.out.println(resultado);
+		
+		
+
+	}
+
+}
