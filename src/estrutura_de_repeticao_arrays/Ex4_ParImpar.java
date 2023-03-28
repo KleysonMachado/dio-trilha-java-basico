98a7cef0a7f3ece0357cfedd60819ef5c9c1df17
KleysonMachado <kleysonm@gmail.com>
2023-03-27 22:53:13 -0300

    Quinto commit do dia 27 de março de 2023.

---
+++ src/estrutura_de_repeticao_arrays/Ex4_ParImpar.java
@@ -0,0 +1,39 @@
+package estrutura_de_repeticao_arrays;
+
+import java.util.Scanner;
+
+/*
+Faça um programa que peça N números inteiros,
+calcule e mostre a quantidade de números pares
+e a quantidade de números impares.
+*/
+
+public class Ex4_ParImpar {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+	
+		int quantNumeros;
+		System.out.println("Quantidade de números: ");
+		quantNumeros = scan.nextInt();
+		int numero = 0;
+		int quantPares = 0;
+		int quantImpares = 0;
+		
+		int count = 0;
+		do {
+			System.out.println("Número: ");
+			numero = scan.nextInt();
+			
+			if (numero % 2 == 0) quantPares++; // quantPares = quantPares + 1;
+			else quantImpares++;
+			
+			//count = count + 1;
+			count++;
+		}while(count < quantNumeros);
+		
+		System.out.println("Quantidade Par: " + quantPares);
+		System.out.println("Quantidade Ímpar: " + quantImpares);
+	}
+
+}
